2388e4612cdb16e2d39c67a71bf2562a9aba6f59
mootqns <mqn360@gmail.com>
2023-05-28 19:59:08 -0700

    fade-in

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
 window.onload = function() {
 
+    // function appear(elm, i, step, speed){
+    //     var t_o;
+    //     //initial opacity
+    //     i = i || 0;
+    //     //opacity increment
+    //     step = step || 5;
+    //     //time waited between two opacity increments in msec
+    //     speed = speed || 50; 
+    
+    //     t_o = setInterval(function(){
+    //         //get opacity in decimals
+    //         var opacity = i / 100;
+    //         //set the next opacity step
+    //         i = i + step; 
+    //         if(opacity > 1 || opacity < 0){
+    //             clearInterval(t_o);
+    //             //if 1-opaque or 0-transparent, stop
+    //             return; 
+    //         }
+    //         //modern browsers
+    //         elm.style.opacity = opacity;
+    //         //older IE
+    //         elm.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+    //     }, speed);
+    // }
+
+    // appear(document.getElementsByTagName('body')[0], 0, 5, 60);
+
+    $("#container").fadeIn("slow");
+
 }
